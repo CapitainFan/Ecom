00cf2b294aab3ae6a3ddc578eb27ccc1f464fbff
Sokolnikov Bogdan <sokolnikovbogdan1@gmail.com>
2024-03-14 22:44:13 +0300

    added shipping form

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -5,10 +5,14 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm, UserInfoForm
+
+from payments.forms import ShippingForm
+from payments.models import ShippingAddress
+
 from django import forms
 from django.db.models import Q
-from cart.cart import Cart
 import json
+from cart.cart import Cart
 
 
 def search(request):
@@ -24,23 +28,78 @@ def search(request):
 		else:
 			return render(request, "search.html", {'searched':searched})
 	else:
-		return render(request, "search.html", {})
+		return render(request, "search.html", {})	
 
 
 def update_info(request):
 	if request.user.is_authenticated:
+		# Get Current User
 		current_user = Profile.objects.get(user__id=request.user.id)
-		form = UserInfoForm(request.POST or None, instance=current_user)
+		# Get Current User's Shipping Info
+		shipping_user = ShippingAddress.objects.get(id=request.user.id)
+		# shipping_user = ShippingAddress.objects.get(id=1)
 
-		if form.is_valid():
+		# Get original User Form
+		form = UserInfoForm(request.POST or None, instance=current_user)
+		# Get User's Shipping Form
+		shipping_form = ShippingForm(request.POST or None, instance=shipping_user)		
+		if form.is_valid() or shipping_form.is_valid():
+			# Save original form
 			form.save()
+			# Save shipping form
+			shipping_form.save()
+
 			messages.success(request, "Your Info Has Been Updated!!")
 			return redirect('home')
-		return render(request, "update_info.html", {'form':form})
+		return render(request, "update_info.html", {'form':form, 'shipping_form':shipping_form})
 	else:
 		messages.success(request, "You Must Be Logged In To Access That Page!!")
 		return redirect('home')
 
+
+
+def update_password(request):
+	if request.user.is_authenticated:
+		current_user = request.user
+		# Did they fill out the form
+		if request.method  == 'POST':
+			form = ChangePasswordForm(current_user, request.POST)
+			# Is the form valid
+			if form.is_valid():
+				form.save()
+				messages.success(request, "Your Password Has Been Updated...")
+				login(request, current_user)
+				return redirect('update_user')
+			else:
+				for error in list(form.errors.values()):
+					messages.error(request, error)
+					return redirect('update_password')
+		else:
+			form = ChangePasswordForm(current_user)
+			return render(request, "update_password.html", {'form':form})
+	else:
+		messages.success(request, "You Must Be Logged In To View That Page...")
+		return redirect('home')
+def update_user(request):
+	if request.user.is_authenticated:
+		current_user = User.objects.get(id=request.user.id)
+		user_form = UpdateUserForm(request.POST or None, instance=current_user)
+
+		if user_form.is_valid():
+			user_form.save()
+
+			login(request, current_user)
+			messages.success(request, "User Has Been Updated!!")
+			return redirect('home')
+		return render(request, "update_user.html", {'user_form':user_form})
+	else:
+		messages.success(request, "You Must Be Logged In To Access That Page!!")
+		return redirect('home')
+
+
+def category_summary(request):
+	categories = Category.objects.all()
+	return render(request, 'category_summary.html', {"categories":categories})	
 
 def category(request, foo):
 	# Replace Hyphens with Spaces
@@ -77,15 +136,20 @@ def login_user(request):
 		if user is not None:
 			login(request, user)
 
+			# Do some shopping cart stuff
 			current_user = Profile.objects.get(user__id=request.user.id)
+			# Get their saved cart from database
 			saved_cart = current_user.old_cart
+			# Convert database string to python dictionary
 			if saved_cart:
+				# Convert to dictionary using JSON
 				converted_cart = json.loads(saved_cart)
+				# Add the loaded cart dictionary to our session
+				# Get the cart
 				cart = Cart(request)
-
-				for key, value in converted_cart.items():
+				# Loop thru the cart and add the items from the database
+				for key,value in converted_cart.items():
 					cart.db_add(product=key, quantity=value)
-
 
 			messages.success(request, ("You Have Been Logged In!"))
 			return redirect('home')
@@ -122,49 +186,3 @@ def register_user(request):
 			return redirect('register')
 	else:
 		return render(request, 'register.html', {'form':form})
-
-
-def category_summary(request):
-	categories = Category.objects.all()
-	return render(request, 'category_summary.html', {"categories":categories})
-
-
-def update_user(request):
-	if request.user.is_authenticated:
-		current_user = User.objects.get(id=request.user.id)
-		user_form = UpdateUserForm(request.POST or None, instance=current_user)
-
-		if user_form.is_valid():
-			user_form.save()
-
-			login(request, current_user)
-			messages.success(request, "User Has Been Updated!!")
-			return redirect('home')
-		return render(request, "update_user.html", {'user_form':user_form})
-	else:
-		messages.success(request, "You Must Be Logged In To Access That Page!!")
-		return redirect('home')
-
-
-def update_password(request):
-	if request.user.is_authenticated:
-		current_user = request.user
-		# Did they fill out the form
-		if request.method  == 'POST':
-			form = ChangePasswordForm(current_user, request.POST)
-			# Is the form valid
-			if form.is_valid():
-				form.save()
-				messages.success(request, "Your Password Has Been Updated...")
-				login(request, current_user)
-				return redirect('update_user')
-			else:
-				for error in list(form.errors.values()):
-					messages.error(request, error)
-					return redirect('update_password')
-		else:
-			form = ChangePasswordForm(current_user)
-			return render(request, "update_password.html", {'form':form})
-	else:
-		messages.success(request, "You Must Be Logged In To View That Page...")
-		return redirect('home')
