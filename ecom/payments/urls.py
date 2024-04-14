c0a7c6ccfe7c291ad65a56d286e0f90d5962f6b7
Sokolnikov Bogdan <sokolnikovbogdan1@gmail.com>
2024-04-14 18:52:24 +0300

    added billing_info page

--- ecom/payments/urls.py
+++ ecom/payments/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('payments_success', views.payments_success, name='payments_success'),
     path('checkout', views.checkout, name='checkout'),
+    path('billing_info', views.billing_info, name='billing_info'),
 ]
